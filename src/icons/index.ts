83166f6ee1702ae7bb02dc0201a3942136ea1edb
hamidrahmani <hr.rahmani@wallex.net>
2025-06-10 01:13:00 +0330

    feat(icons): add ExclamationMarkTriangleFill and StarOn icons, update index export with new icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,7 @@
 export { default as FilmIcon } from "./Film.svg?react";
-export { default as StarOffIcon } from "./StarOff.svg?react";
 export { default as CloseIcon } from "./Close.svg?react";
+export { default as StarOffIcon } from "./StarOff.svg?react";
+export { default as StarOnIcon } from "./StarOn.svg?react";
+export { default as ExclamationMarkTriangleFillIcon } from "./ExclamationMarkTriangleFill.svg?react";
+export { default as VisibilityOnIcon } from "./VisibilityOn.svg?react";
+export { default as VisibilityOffIcon } from "./VisibilityOff.svg?react";
