077d58332d530932bef4d207389d7850bdba99f9
hamidrahmani <hr.rahmani@wallex.net>
2025-06-10 00:28:17 +0330

    feat(icons): add Close, Film, and StarOff icons with SVG files and update index export

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,1 +1,3 @@
-export {};
+export { default as FilmIcon } from "./Film.svg?react";
+export { default as StarOffIcon } from "./StarOff.svg?react";
+export { default as CloseIcon } from "./Close.svg?react";
